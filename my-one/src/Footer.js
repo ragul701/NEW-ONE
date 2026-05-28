9d14c211c63231b29b0cf54b6cc673aaf3d80615
Ragul <96213306@gmail.com>
2026-05-28 17:00:35 +0530

    fixed node_modules

--- my-one/src/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer>
-      <p>© 2026 Ragul Social Media | All Rights Reserved</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- my-one/src/Missing.js
+++
@@ -1,5 +0,0 @@
-function Missing() {
-  return <h1>Missing</h1>;
-}
-
-export default Missing;
